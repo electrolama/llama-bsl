f68c7e46faf4ee6898c0cbbf24bd4c41ee529a63
omerk <omerk@users.noreply.github.com>
2021-09-10 22:35:59 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from setuptools import setup
 
 
 setup(
-    name="cc2538-bsl",
-    description="Script to communicate with Texas Instruments CC13xx/CC2538/CC26xx Serial Boot Loader .",
+    name="llama-bsl",
+    description="Script to communicate with Texas Instruments CC13xx/CC2538/CC26xx Serial Boot Loader (Fork of cc2538-bsl.py)",
     long_description=open("README.md", encoding="utf-8").read(),
     keywords="cc2538, cc1310, cc13xx, bootloader, cc26xx, cc2650, cc2640",
-    url="https://github.com/JelmerT/cc2538-bsl",
-    author="Jelmer Tiete",
-    author_email="jelmer@tiete.be",
+    url="https://github.com/electrolama/llama-bsl",
+    author="Omer Kilic",
+    author_email="omerkilic@gmail.com",
     license="BSD-3-Clause",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
     ],
@@ -32,5 +33,5 @@ setup(
         '': ["intelhex"],
         'intelhex': ["python-magic"]
     },
-    scripts=["cc2538-bsl.py"],
+    scripts=["llama-bsl"],
 )
